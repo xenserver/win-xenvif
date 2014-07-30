790290efe531ac6a1eca8792f93646e14d01b022
Paul Durrant <paul.durrant@citrix.com>
2014-07-30 16:25:49 +0100

    Revert commit 3714613096b1bb7b7a76e9d360106ea19208f243
    >    Change Cache lifecycle to match ring lifecycle.
    >    Ensure caches are destroyed when device transitions to D3 to prevent
    >    assertion failure in xenbus.
    >
    >    Signed-off-by: Owen Smith <owen.smith@citrix.com>
    
    Signed-off-by: Paul Durrant <paul.durrant@citrix.com>

--- src/xenvif/granter.c
+++ src/xenvif/granter.c
@@ -101,6 +101,8 @@ GranterInitialize(
     OUT PXENVIF_GRANTER     *Granter
     )
 {
+    CHAR                    Name[MAXNAMELEN];
+    ULONG                   Index;
     NTSTATUS                status;
 
     *Granter = __GranterAllocate(sizeof (XENVIF_GRANTER));
@@ -113,10 +115,44 @@ GranterInitialize(
 
     GNTTAB(Acquire, (*Granter)->GnttabInterface);
 
-    (*Granter)->Frontend = Frontend;
+    status = RtlStringCbPrintfA(Name,
+                                sizeof (Name),
+                                "%s",
+                                FrontendGetPath(Frontend));
+    if (!NT_SUCCESS(status))
+        goto fail2;
+
+    for (Index = 0; Name[Index] != '\0'; Index++)
+        if (Name[Index] == '/')
+            Name[Index] = '_';
+
     KeInitializeSpinLock(&(*Granter)->Lock);
 
+    status = GNTTAB(CreateCache,
+                    (*Granter)->GnttabInterface,
+                    Name,
+                    0,
+                    GranterAcquireLock,
+                    GranterReleaseLock,
+                    *Granter,
+                    &(*Granter)->Cache);
+    if (!NT_SUCCESS(status))
+        goto fail3;
+
+    (*Granter)->Frontend = Frontend;
+
     return STATUS_SUCCESS;
+
+fail3:
+    Error("fail2\n");
+
+    RtlZeroMemory(&(*Granter)->Lock, sizeof (KSPIN_LOCK));
+
+fail2:
+    Error("fail2\n");
+
+    GNTTAB(Release, (*Granter)->GnttabInterface);
+    (*Granter)->GnttabInterface = NULL;
 
 fail1:
     Error("fail1 (%08x)\n", status);
@@ -129,43 +165,9 @@ GranterConnect(
     IN  PXENVIF_GRANTER     Granter
     )
 {
-    CHAR                    Name[MAXNAMELEN];
-    ULONG                   Index;
-    NTSTATUS                status;
-
-    status = RtlStringCbPrintfA(Name,
-                                sizeof (Name),
-                                "%s",
-                                FrontendGetPath(Granter->Frontend));
-    if (!NT_SUCCESS(status))
-        goto fail1;
-
-    for (Index = 0; Name[Index] != '\0'; Index++)
-        if (Name[Index] == '/')
-            Name[Index] = '_';
-
-    ASSERT3P(Granter->Cache, ==, NULL);
-
-    status = GNTTAB(CreateCache,
-                    Granter->GnttabInterface,
-                    Name,
-                    0,
-                    GranterAcquireLock,
-                    GranterReleaseLock,
-                    Granter,
-                    &Granter->Cache);
-    if (!NT_SUCCESS(status))
-        goto fail2;
+    UNREFERENCED_PARAMETER(Granter);
 
     return STATUS_SUCCESS;
-
-fail2:
-    Error("fail2\n");
-
-fail1:
-    Error("fail1 (%08x)\n", status);
-
-    return status;
 }
 
 NTSTATUS
@@ -191,8 +193,6 @@ GranterPermitAccess(
     NTSTATUS                    status;
 
     Frontend = Granter->Frontend;
-
-    ASSERT3P(Granter->Cache, !=, NULL);
 
     status = GNTTAB(PermitForeignAccess,
                     Granter->GnttabInterface,
@@ -222,8 +222,6 @@ GranterRevokeAccess(
 {
     PXENBUS_GNTTAB_DESCRIPTOR   Descriptor = Handle;
     NTSTATUS                    status;
-
-    ASSERT3P(Granter->Cache, !=, NULL);
 
     status = GNTTAB(RevokeForeignAccess,
                     Granter->GnttabInterface,
@@ -267,12 +265,7 @@ GranterDisconnect(
     IN  PXENVIF_GRANTER     Granter
     )
 {
-    ASSERT3P(Granter->Cache, !=, NULL);
-
-    GNTTAB(DestroyCache,
-           Granter->GnttabInterface,
-           Granter->Cache);
-    Granter->Cache = NULL;
+    UNREFERENCED_PARAMETER(Granter);
 }
 
 VOID
@@ -281,6 +274,11 @@ GranterTeardown(
     )
 {
     Granter->Frontend = NULL;
+
+    GNTTAB(DestroyCache,
+           Granter->GnttabInterface,
+           Granter->Cache);
+    Granter->Cache = NULL;
 
     RtlZeroMemory(&Granter->Lock, sizeof (KSPIN_LOCK));
 
